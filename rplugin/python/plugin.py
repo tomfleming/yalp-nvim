98a0df3f2a873cd1f0fc2fb0a91efa101d89e2b3
Tom Fleming <tomdfleming@gmail.com>
2016-10-27 00:18:29 -0700

    attempt to make html on writes

--- rplugin/python/plugin.py
+++ rplugin/python/plugin.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+from subprocess import check_output
 from multiprocessing import Process
 
 import neovim
@@ -12,23 +13,25 @@ HERE = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
 sys.path.insert(0, HERE)
 import sio
 
+
 @neovim.plugin
 class TestPlugin(object):
     """Plugin for live-previewing RST (and other?) documents"""
 
     def __init__(self, nvim):
         self.nvim = nvim
+        self.makefile_dir = os.path.expanduser(self.nvim.command_output('pwd'))
+        self.makefile = os.path.join(self.makefile_dir, 'Makefile')
+        self.has_makefile = os.path.isfile(self.makefile)
 
         proc = Process(target=sio.runserver)
         proc.start()
 
     @neovim.autocmd('CursorMoved,CursorMovedI',
                     pattern='*.rst',
-                    eval='expand("<afile>")',
-                    sync=True)
+                    eval='expand("<afile>")')
     def on_edit(self, filename):
         """Send buffer contents to socketio server via PUT request"""
-        self.nvim.out_write("Stuff and junk")
         buf = self.nvim.current.buffer
         html = publish_string("\n".join(buf[:]),
                               writer_name='html',
@@ -38,3 +41,31 @@ class TestPlugin(object):
         except Exception, err:
             print filename
             print err
+
+    @neovim.autocmd('BufWritePost',
+                    pattern='*.rst',
+                    eval='expand("<afile>")')
+    def make_html(self, filename):
+        """Send html contents to socketio server via PUT request"""
+        if self.has_makefile:
+            try:
+                check_output(['make', 'html'], cwd=self.makefile_dir)
+                requests.put("http://localhost:8000/render", filename)
+            except Exception, err:
+                print filename
+                try:
+                    requests.put("http://localhost:8000/render", err)
+                except:
+                    print err
+
+    def parse_makefile(self):
+        """Retrieve options from sphinx makefile"""
+        if self.has_makefile:
+            with open(self.makefile, 'r') as mf:
+                options = [line for line in mf.readlines() if '=' in line]
+            options = dict([
+                (l[0].strip(), " ".join(l[2:]).strip())
+                for l in map(str.split, options, "=")])
+            return options
+        else:
+            return {}
